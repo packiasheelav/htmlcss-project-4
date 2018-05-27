29de01ed6ff07bbf97c87fc37a86f3023813210e
psv2018 <sheelaveldurai@gmail.com>
2018-05-27 20:03:04 +0300

    First commit

---
+++ jscript.js
@@ -0,0 +1,10 @@
+
+//document.getElementById("nameValidation").addEventListener("blur", myFunction);
+
+console.log( x);
+// function myFunction(){
+//     const regx=/[a-zA-Z]{5,}/;
+//     alert("Input field lost focus");
+// }
+
+
